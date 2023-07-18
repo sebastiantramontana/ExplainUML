fab0cf97f63a96df5a175307aab729c1d9ea3119
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2023-07-18 17:41:03 -0300

    Remove obsolete files

--- ExplainUml.Shapes/Templates/Trasncluders/ISvgMethodsTranscluder.cs
+++
@@ -1,9 +0,0 @@
-﻿using ExplainUml.BuildingBlocks;
-
-namespace ExplainUml.Shapes.Templates.Trasncluders
-{
-    internal interface ISvgMethodsTranscluder
-    {
-        string Tranclude(IEnumerable<Method> methods, string templateContent);
-    }
-}
